d9da337ca26bbb6d37bfb0903065b188f640d10a
chinmay <chinmaykulkarni20@gmail.com>
2021-02-17 19:27:30 +0000

    Test feature

--- src/test/java/features/placeValidations.feature
+++ src/test/java/features/placeValidations.feature
@@ -1,3 +1,7 @@
 Feature: To Test Validations
 
   Scenario: Test 1
+    Given I am a user
+    When I open the app
+    And I click
+    Then I see the content
---
+++ src/test/java/stepDefinations/PlaceValidations.java
@@ -0,0 +1,30 @@
+package stepDefinations;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class PlaceValidations {
+
+    @When("^I open the app$")
+    public void iOpenTheApp() {
+
+    }
+
+    @Given("^I am a user$")
+    public void iAmAUser() throws Throwable {
+
+    }
+
+    @And("^I click$")
+    public void iClick() throws Throwable {
+
+    }
+
+    @Then("^I see the content$")
+    public void iSeeTheContent() throws Throwable {
+
+    }
+}
